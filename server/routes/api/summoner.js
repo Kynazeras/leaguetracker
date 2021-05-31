af96e7b34ca9a9cf11a5c08cde1826a068e5397f
Seth Holloway <the.seth.holloway@gmail.com>
2021-05-31 18:41:57 -0400

    added ranked info and item containers

--- server/routes/api/summoner.js
+++ server/routes/api/summoner.js
@@ -6,11 +6,25 @@ const axios = require("axios");
 const env = require("dotenv").config();
 const apiKeyString = `?api_key=${process.env.RIOT_API_KEY}`;
 
-router.get("/:summonerName", (req, res) => {
+router.get("/details/:summonerName", (req, res) => {
   const { summonerName } = req.params;
   axios
     .get(
       `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}${apiKeyString}`
+    )
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
+
+router.get("/rank/:summonerId", (req, res) => {
+  const { summonerId } = req.params;
+  axios
+    .get(
+      `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}${apiKeyString}`
     )
     .then((response) => {
       res.json(response.data);
